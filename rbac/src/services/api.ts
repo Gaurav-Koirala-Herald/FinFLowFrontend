7393d9ca31f644fc077efd995e7744603844ea90
gauravK0i <gaurav.koirala@globalsquareit.com>
2025-12-30 13:14:39 +0545

    changed the api url

--- rbac/src/services/api.ts
+++ rbac/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "https://localhost:7166/api"
+const API_BASE_URL = import.meta.env.VITE_API_URL || "https://api.koiralagaurav.com.np/api"
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
